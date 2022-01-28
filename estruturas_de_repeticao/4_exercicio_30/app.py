e47825be3aea4cf3f30c0e698a8bfd29e46c9405
Ticiane Silva <test.ticianesilva@gmail.com>
2022-01-28 09:45:33 -0300

    Exercício 3 While

---
+++ estruturas_de_repeticao/4_exercicio_30/app.py
@@ -0,0 +1,10 @@
+# Crie um loop em que o fim é determinado pelo número que o usuário insere no sistema;
+# Imprima os números com print;
+
+fim = int(input("Digite um número: "))
+
+inicio = 0
+
+while inicio<= fim:
+  print(inicio)
+  inicio = inicio + 1
