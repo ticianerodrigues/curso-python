1f300dc94174d829d8141c0e7e4e6f6995462549
Ticiane Silva <test.ticianesilva@gmail.com>
2022-01-31 10:10:31 -0300

    Acessando e modificando elementos

---
+++ listas/2_acessando_modifidando/app.py
@@ -0,0 +1,19 @@
+numeros = [1, 2, 3, 4, 5]
+
+print(numeros[0])
+
+print(numeros[3])
+
+nomes = ["Paulo", "José", "Felipe"]
+
+nomes[1] = "João"
+
+print(nomes)
+
+print("O meu nome é %s" % nomes[1])
+
+nome = nomes[1]
+
+print(nome)
+
+
