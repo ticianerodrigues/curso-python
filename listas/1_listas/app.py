a010b9d4cf008a4e95a425ec5ce50eeec438f269
Ticiane Silva <test.ticianesilva@gmail.com>
2022-01-31 09:50:19 -0300

    Aula sobre listas

---
+++ listas/1_listas/app.py
@@ -0,0 +1,14 @@
+lista = []
+
+print(lista)
+print(type(lista))
+
+numeros = [1, 2, 3, 4, 5]
+print(numeros)
+
+nomes= ["João", "Maria", "Paulo"]
+print(nomes)
+print(type(nomes))
+
+booleans = [True, False, True]
+print(booleans)
