f9bc5a4c28e123a190d1d4ae043659349bb8b7be
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-01 15:08:20 -0300

    Exercício 1 loop baseado na função len

---
+++ listas/12_exercicio_39/app.py
@@ -0,0 +1,13 @@
+# Crie uma lista com 5 números;
+# Faça um loop baseado na função len;
+# Imprima na tela cada número somando a 10;
+
+lista = [22,33,44,55,66]
+
+i = 0
+
+while i < len(lista):
+  print(lista[i] + 10)
+  i = i + 1
+
+
