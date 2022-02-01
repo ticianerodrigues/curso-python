2cf61b3c4b03fbf5dc23f5ad6f8131ff753e590d
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-01 10:30:57 -0300

    Exercício 1 fatiamento de listas

---
+++ listas/9_exercicio_38/app.py
@@ -0,0 +1,9 @@
+# Crie uma lista com 10 elementos;
+# Por meio de fatiamento extraia do terceiro ao sétimo;
+# Atribua esta nova lista a uma variável e a exiba no terminal;
+
+lista = [0,1,2,3,4,5,6,7,8,9]
+
+nova_lista = lista[2:7]
+
+print(nova_lista)
