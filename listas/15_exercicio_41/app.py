139901c33f6d8f20696e8c1f6a74176df0c7322b
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-14 10:05:26 -0300

    Exercício 2 append

---
+++ listas/15_exercicio_41/app.py
@@ -0,0 +1,22 @@
+A = [1, 2, 3, 4]
+
+B = [5, 6, 7, 8]
+
+C = []
+
+i = 0
+
+while i < len(A):
+  C.append(A[i])
+
+  i = i + 1
+
+j= 0
+
+while j < len(B):
+  C.append(B[j])
+  j = j + 1
+
+print (C)  
+
+
