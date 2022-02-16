2ff788e37ce200cf549c19328fa0982a0f9dd863
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-16 10:41:28 -0300

    Dicionários

---
+++ listas/30_dicionarios/app.py
@@ -0,0 +1,14 @@
+carros ={
+  "Portas": 4,
+  "Janelas": 4,
+  "Motor": 2.0,
+  "Telo_Solar": True,
+  "Cambio": "Automatico"
+}
+
+print(carros)
+
+print(carros["Motor"])
+
+print(carros["Cambio"])
+
