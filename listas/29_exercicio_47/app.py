636e72748f3ab8643ea041661719a377ff9356d3
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-16 10:19:02 -0300

    Exercício utilizando listas dentro de listas

---
+++ listas/29_exercicio_47/app.py
@@ -0,0 +1,16 @@
+produto_a = ["Blusa", "Rosa", 50]
+produto_b = ["Vestido", "Preto", 120]
+produto_c = ["Saia", "Amarela", 35]
+
+print(produto_a, produto_b, produto_c)
+
+produtos = [produto_a, produto_b, produto_c]
+
+print(produtos[0][1])
+
+print(produtos[2][2])
+
+print(produtos[2][0])
+
+for produtos in produtos:
+  print("O produto é: %s" % produtos[0])
