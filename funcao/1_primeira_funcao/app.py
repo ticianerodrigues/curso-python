05d4c9c47a07513dfcac52cc18ebf3a1a6c401a7
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-17 10:03:29 -0300

    Funções

---
+++ funcao/1_primeira_funcao/app.py
@@ -0,0 +1,17 @@
+def digaOi(nome):
+  print("Olá %s!" % nome)
+
+digaOi("Fernanda")
+
+digaOi("Paulo")
+
+if 10 > 5:
+  digaOi("João")
+
+digaOi("José")
+
+def digaOla():
+  print("Olá Mundo!")
+  print("Olá Fernanda")
+
+digaOla()
