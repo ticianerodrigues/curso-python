a8619ea70a672bc0378fa6e828a8881e93541856
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-17 09:23:00 -0300

    Exercício adicionando  e deletando

---
+++ listas/34_exercicio_49/app.py
@@ -0,0 +1,19 @@
+carro = {
+  "pneus": 4,
+  "portas": 2,
+  "motor": 1,
+  "janelas": 4
+}
+
+print(carro)
+
+carro["teto_solar"] = 1
+
+print(carro)
+
+del carro["motor"]
+del carro["janelas"]
+
+print(carro)
+
+
