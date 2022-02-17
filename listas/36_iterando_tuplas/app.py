5b23f92b47354b91da1e3637d8c319273b5d367b
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-17 09:39:20 -0300

    Iterando em tuplas

---
+++ listas/36_iterando_tuplas/app.py
@@ -0,0 +1,15 @@
+tupla = ("Maria", "Fernanda", "Paulo")
+
+for nome in tupla:
+  print(nome)
+
+  if nome == "Fernanda":
+   print("Parabéns Fernanda!")
+
+i = 0
+
+while i < len(tupla):
+  print(tupla[i])
+  i = i + 1
+
+
