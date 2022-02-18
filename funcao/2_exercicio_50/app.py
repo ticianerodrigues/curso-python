a7873e8a60a33eeb742962b3efed56a4f545400b
Ticiane Silva <test.ticianesilva@gmail.com>
2022-02-18 10:10:23 -0300

    Exercício funcão (def)

---
+++ funcao/2_exercicio_50/app.py
@@ -0,0 +1,15 @@
+def soma(a, b):
+  print(a + b)
+
+def multiplicacao(a, b):
+  print(a * b)
+
+multiplicacao(2, 4)
+
+num_a = 4
+num_b = 8
+
+multiplicacao(num_a, num_b)
+multiplicacao(num_b, num_b)
+multiplicacao(num_a, 15)
+multiplicacao(20, 40)
